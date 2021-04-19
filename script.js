dfdff742a88f8f631f9844a6420cbe0a3254e9c0
alirezadalvand <alireza.d1339@gmail.com>
2021-04-20 01:27:40 +0430

    array some every

---
+++ script.js
@@ -0,0 +1,9 @@
+const number = [12, 4, 18, 20, 15, 17];
+const nombre = ["Bahar", "Hanie", "Ayda", "Alireza"];
+
+const pass = (element) => element > 15;
+const failed = (element) => element > 5;
+
+console.log(number.every(failed));
+
+// console.log(sum);
